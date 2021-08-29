96b630e6607a8786043a5e47d1db139a301d738f
Paysure.Digital <paysure.digital@PaysureDigitals-MacBook-Pro.local>
2021-08-29 13:05:40 +0100

    update

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
-const express = require('express');
-const path = require('path');
-require('dotenv').config();
+const express = require("express");
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
 
+// Constants
+const cert = fs.readFileSync("./ssl/cert.pem");
+const ca = fs.readFileSync("./ssl/bundle.crt");
+const key = fs.readFileSync("./ssl/key.pem");
+const options = {
+  key,
+  cert,
+  ca,
+};
+
+
+// const HOST=process.env.HOST||"192.168.253.53"
+// App
+const app = express();
+const httpsServer = https.createServer(options, app);
 // Constants
 const PORT = process.env.PORT || 8080;
 
 // App
-const app = express();
-
 app.use(express.static('dist'));
 // app.use(express.static('dist'));
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve('dist/dist/index.html'));
+    res.sendFile(path.resolve('dist/index.html'));
 });
 
-app.listen(PORT);
+
+httpsServer.listen(PORT);
+// app.listen(PORT);
 console.log(`Running on http://localhost:${PORT}`);
