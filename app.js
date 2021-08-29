51266e787aa35eedf8ea0c3d6aad86af0fb7ef81
teembet <51920737+teembet@users.noreply.github.com>
2021-08-29 13:02:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.static('dist'));
 // app.use(express.static('dist'));
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve('dist/dist/index.html'));
+    res.sendFile(path.resolve('dist/index.html'));
 });
 
 app.listen(PORT);
